e0d3a600bf593c1cf9bc071e968062cf813bba0d
Sam <sam82520.work@gmail.com>
2019-09-19 23:20:35 +0800

    Init

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  watch: true,
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+//   devServer: {
+//     contentBase: path.join(__dirname, 'dist'),
+//     compress: true
+//   }
+};
